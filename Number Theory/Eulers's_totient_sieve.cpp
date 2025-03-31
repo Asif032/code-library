ee0aa3dbe037b42a84fe36f3a402e072e869c8b5
Asif032 <asif01981872223@gmail.com>
2025-03-31 22:57:03 +0600

    fix eulers-totient-sieve

--- Number Theory/Eulers's_totient_sieve.cpp
+++ Number Theory/Eulers's_totient_sieve.cpp
@@ -8,7 +8,7 @@ void sieve() {
   for (int i = 0; i < N; i++) {
     phi[i] = i;
   }
-  for (int i = 2; i * i < N; i++) {
+  for (int i = 2; i < N; i++) {
     if (phi[i] == i) {
       for (int j = i; j < N; j += i) {
         phi[j] -= phi[j] / i;
